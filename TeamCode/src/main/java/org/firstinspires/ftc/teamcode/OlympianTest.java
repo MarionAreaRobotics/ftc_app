0a3452246727ebf9483763d3f014f91c041d885f
marsrobotics <marsroboticsclub@gmail.com>
2016-10-11 07:06:47 -0500

    More drive controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OlympianTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OlympianTest.java
@@ -1,8 +1,94 @@
 package org.firstinspires.ftc.teamcode;
 
-/**
- * Created by mars on 9/21/16.
- */
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-public class OlympianTest {
+@TeleOp(name="Olympian", group="Testing")
+public class OlympianTest extends OpMode {
+
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftMotor1 = null;
+    private DcMotor leftMotor2 = null;
+    private DcMotor rightMotor1 = null;
+    private DcMotor rightMotor2 = null;
+    private double motorThreshold = 0.065;
+
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initialized");
+
+        leftMotor1 = hardwareMap.dcMotor.get("left motor 1");
+        leftMotor2 = hardwareMap.dcMotor.get("left motor 2");
+        rightMotor1 = hardwareMap.dcMotor.get("right motor 1");
+        rightMotor2 = hardwareMap.dcMotor.get("right motor 2");
+
+        // leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+
+        telemetry.addData("Status", "Initialized");
+    }
+
+
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+        // Driving control "telemetry"
+        telemetry.addData("Status", "Running: " + runtime.toString());
+        telemetry.addData("RightX", gamepad1.right_stick_x);
+        telemetry.addData("RightY", gamepad1.right_stick_y);
+        telemetry.addData("LeftX", gamepad1.left_stick_x);
+        telemetry.addData("LeftY", gamepad1.left_stick_y);
+
+        float leftX = -gamepad1.left_stick_x;
+        float rightX = -gamepad1.right_stick_x;
+        float leftY = -gamepad1.left_stick_y;
+        float rightY = -gamepad1.right_stick_y;
+
+        float coord = leftY;
+        DcMotor.Direction dir1 = DcMotor.Direction.REVERSE;
+        DcMotor.Direction dir2 = DcMotor.Direction.REVERSE;
+        DcMotor.Direction dir3 = DcMotor.Direction.FORWARD;
+        DcMotor.Direction dir4 = DcMotor.Direction.FORWARD;
+
+        if ((leftX >= motorThreshold) || (leftX <= -motorThreshold)) {
+            dir1 = DcMotor.Direction.FORWARD;
+            dir4 = DcMotor.Direction.REVERSE;
+            coord = leftX;
+        } else if((rightX >= motorThreshold) || (rightX <= -motorThreshold)){
+            dir1 = DcMotor.Direction.FORWARD;
+            dir2 = DcMotor.Direction.FORWARD;
+            coord = rightX;
+        }
+        rightMotor1.setDirection(dir1);// Set to FORWARD if using AndyMark motors
+        rightMotor2.setDirection(dir2);
+        leftMotor1.setDirection(dir3);
+        leftMotor2.setDirection(dir4);
+        leftMotor1.setPower(coord);
+        leftMotor2.setPower(coord);
+        rightMotor1.setPower(coord);
+        rightMotor2.setPower(coord);
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TitanTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TitanTest.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name="Titan", group="Testing")
@@ -16,10 +17,13 @@ public class TitanTest extends OpMode
     private DcMotor rightMotor1 = null;
     private DcMotor rightMotor2 = null;
 
-    @Override
-    public void init() {
-        telemetry.addData("Status", "Initialized");
+    private DcMotor leftLinearSlide = null;
+    private DcMotor rightLinearSlide = null;
 
+    private Servo leftArmServo = null;
+    private Servo rightArmServo = null;
+
+    public void driveInit() {
         leftMotor1  = hardwareMap.dcMotor.get("left motor 1");
         leftMotor2  = hardwareMap.dcMotor.get("left motor 2");
         rightMotor1 = hardwareMap.dcMotor.get("right motor 1");
@@ -28,7 +32,28 @@ public class TitanTest extends OpMode
         // leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
         rightMotor1.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
         rightMotor2.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    public void linearSlidesInit() {
+        leftLinearSlide = hardwareMap.dcMotor.get("left linear slide");
+        rightLinearSlide = hardwareMap.dcMotor.get("right linear slide");
+
+        rightLinearSlide.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    public void armServosInit() {
+        leftArmServo = hardwareMap.servo.get("left arm servo");
+        rightArmServo = hardwareMap.servo.get("right arm servo");
+
+        rightArmServo.setDirection(Servo.Direction.REVERSE);
+    }
+
+    @Override
+    public void init() {
         telemetry.addData("Status", "Initialized");
+        driveInit();
+        linearSlidesInit();
+        armServosInit();
     }
 
 
@@ -47,16 +72,38 @@ public class TitanTest extends OpMode
     /*
      * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
      */
-    @Override
-    public void loop() {
-        telemetry.addData("Status", "Running: " + runtime.toString());
+
+    public void driveLoop() {
 
         leftMotor1.setPower(-gamepad1.left_stick_y);
         leftMotor2.setPower(-gamepad1.left_stick_y);
         rightMotor1.setPower(-gamepad1.right_stick_y);
         rightMotor2.setPower(-gamepad1.right_stick_y);
-        //leftMotor.setPower(-gamepad1.right_stick_y);
-        //rightMotor.setPower(-gamepad1.left_stick_y);
+
+    }
+
+    public void linearSlidesLoop() {
+
+        leftLinearSlide.setPower(-gamepad2.left_stick_y);
+        rightLinearSlide.setPower(-gamepad2.left_stick_y);
+
+    }
+
+    public void armServosLoop() {
+
+        if(gamepad1.a){
+            rightArmServo.setPosition(1);
+        }
+
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("Status", "Running: " + runtime.toString());
+
+        driveLoop();
+        linearSlidesLoop();
+        armServosLoop();
     }
 
     /*
