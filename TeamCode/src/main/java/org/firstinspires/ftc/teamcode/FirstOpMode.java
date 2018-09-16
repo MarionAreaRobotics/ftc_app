15555025e743abda82a0873d57c912fab75948d5
marsrobotics <marsroboticsclub@gmail.com>
2018-09-16 16:19:26 -0500

    From 9/16 meeting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstOpMode.java
@@ -1,37 +1,35 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorController;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@Disabled
 @TeleOp(name="First OpMode", group="Testing")
 public class FirstOpMode extends OpMode
 {
 
     private ElapsedTime runtime = new ElapsedTime();
 
-    private DcMotor leftMotor = null;
-    private DcMotor rightMotor = null;
+    private DcMotor leftMotor1 = null;
+    private DcMotor leftMotor2 = null;
+    private DcMotor rightMotor1 = null;
+    private DcMotor rightMotor2 = null;
 
-    private Servo servo = null;
+    private double motorThreshold = 0.065;
+
+    //private Servo servo = null;
 
     @Override
     public void init() {
         telemetry.addData("Status", "Initialized");
 
-        leftMotor  = hardwareMap.dcMotor.get("left motor");
-        rightMotor = hardwareMap.dcMotor.get("right motor");
-
-        servo = hardwareMap.servo.get("servo");
-        servo.setPosition(0);
+        leftMotor1 = hardwareMap.dcMotor.get("left motor 1");
+        leftMotor2 = hardwareMap.dcMotor.get("left motor 2");
+        rightMotor1 = hardwareMap.dcMotor.get("right motor 1");
+        rightMotor2 = hardwareMap.dcMotor.get("right motor 2");
 
         //leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
-        rightMotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
         telemetry.addData("Status", "Initialized");
     }
 
@@ -55,21 +53,36 @@ public class FirstOpMode extends OpMode
     public void loop() {
         telemetry.addData("Status", "Running: " + runtime.toString());
 
-        leftMotor.setPower(-gamepad1.right_stick_y);
-        rightMotor.setPower(-gamepad1.left_stick_y);
+        float leftX = -(gamepad1.left_stick_x/2);// assigning controller values to a variable
+        float rightX = -(gamepad1.right_stick_x/2);
+        float leftY = -(gamepad1.left_stick_y/2);
+        float rightY = -(gamepad1.right_stick_y/2);
 
-        if (gamepad1.a) {
-            if (servo.getPosition() != 1) {
-                servo.setPosition(1);
+        float coord1 = leftY;
+        float coord = rightY;
+        DcMotor.Direction dir1 = DcMotor.Direction.REVERSE;
+        DcMotor.Direction dir2 = DcMotor.Direction.FORWARD;
+        DcMotor.Direction dir3 = DcMotor.Direction.FORWARD;
+        DcMotor.Direction dir4 = DcMotor.Direction.REVERSE;
 
-            }
-        } else {
-            if (servo.getPosition() != 0) {
-                servo.setPosition(0);
-            }
+        if (((leftX >= motorThreshold) || (leftX <= -motorThreshold)) && ((rightX >= motorThreshold) || (rightX <= -motorThreshold))) {
+            dir1 = DcMotor.Direction.FORWARD;
+            dir2 = DcMotor.Direction.FORWARD;
+            dir3 = DcMotor.Direction.FORWARD;
+            dir4 = DcMotor.Direction.FORWARD;
+            coord1 = leftX;
+            coord = rightX;
         }
 
-        telemetry.addData("Servo Position: ", servo.getPosition());
+        leftMotor1.setDirection(dir1);// Set to FORWARD if using AndyMark motors
+        leftMotor2.setDirection(dir2);
+        rightMotor1.setDirection(dir3);
+        rightMotor2.setDirection(dir4);
+        leftMotor1.setPower(coord1);
+        leftMotor2.setPower(coord1);
+        rightMotor1.setPower(coord);
+        rightMotor2.setPower(coord);
+
     }
 
     /*
